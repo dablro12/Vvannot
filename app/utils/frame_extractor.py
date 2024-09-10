def257fab6b1fa195f7423e463b7479114cfc4d6
dablro12 <dablro1232@gmail.com>
2024-09-10 21:54:43 +0900

    add seg-model

--- app/utils/frame_extractor.py
+++ app/utils/frame_extractor.py
@@ -2,16 +2,19 @@ import cv2
 
 def extract_first_frame(video_path):
     cap = cv2.VideoCapture(video_path)
+    width, height = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    
     ret, frame = cap.read()
+    
     cap.release()
     if ret:
-        return frame
+        return frame, (width, height)
     else:
         return None
 
-def crop_video(video_path):
-    """
-    진행중
-    """
-    cap = cv2.VideoCapture(video_path)
-    ret, frame = cap.read()
+def extract_position(df):
+    left = df['left'].values[0] * 2
+    top = df['top'].values[0] * 2
+    width = df['width'].values[0] * 2
+    height = df['height'].values[0] * 2
+    return (left, top, width, height)
