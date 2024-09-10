55c917f489501bd9c71fd1ec92b94584c1e865bd
dablro12 <dablro1232@gmail.com>
2024-09-10 21:54:43 +0900

    add seg-model

--- model/st_tracker.py
+++ model/st_tracker.py
@@ -7,7 +7,6 @@ from deep_sort_realtime.deepsort_tracker import DeepSort
 import csv
 import streamlit as st
 from model.utils.resize import img_cropper
-from ultralytics import SAM
 
 GREEN = (0, 255, 0)
 WHITE = (255, 255, 255)
@@ -22,7 +21,6 @@ class ObjectTracker:
         self.model, self.tracker, self.coco128_class = self.init_tracker(model_path, cocolabel)
         self.csv_file, self.csv_writer = self.init_save_csv(save_path)
         self.save_path = save_path  # Initialize self.save_path
-        self.seg_model = self.init_segmentation(model_name = 'weights/sam2_t.pt')
         
     def init_video(self, video_path, save_path):
         cap = cv2.VideoCapture(video_path)
@@ -44,11 +42,7 @@ class ObjectTracker:
         out = cv2.VideoWriter(save_path, fourcc, FPS, (WIDTH, HEIGHT))
         print('#### [C] init video')
         return cap, out
-    def init_segmentation(self, model_name:str):
-        model = SAM(model_name)
-        return model
         
-    
     def init_tracker(self, model_path, cocolabel):
         model = YOLO(model_path)
         tracker = DeepSort(max_age=30, n_init=1)
@@ -94,16 +88,8 @@ class ObjectTracker:
                 results.append([[xmin, ymin, xmax - xmin, ymax - ymin], confidence, class_id])
                 print(f"results : {results}")
         return results
-    def human_segmentation(self, frame, bbox):
-        results = self.seg_model(frame, bboxes=[bbox])
-        # frame에 마스크 표시
-        for result in results:
-            mask = result.masks.xy
-            mask = [np.array(m, dtype=np.int32) for m in mask]  # Ensure mask is in the correct format
-            cv2.fillPoly(frame, mask, (0, 0, 255))  # Using red color for the mask
-        
-        
-    def human_tracker(self, results, frame, original_frame, crop_position):
+    
+    def human_tracker(self, results, frame, crop_position):
         tracks = self.tracker.update_tracks(results, frame=frame)
         
         for track in tracks:
@@ -118,17 +104,23 @@ class ObjectTracker:
                 ltrb = track.to_ltrb()
                 xmin, ymin, xmax, ymax = int(ltrb[0]), int(ltrb[1]), int(ltrb[2]), int(ltrb[3])
                 
-                # sam2 모델로 사람 영역 추출
-                self.human_segmentation(frame, bbox = (xmin, ymin, xmax, ymax))
-                
-                # 사람 표시
+                # 사람 표시 (크롭된 프레임)
                 cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), GREEN, 2)
                 cv2.putText(frame, f'ID: {track.track_id}', (xmin + 5, ymin - 8), cv2.FONT_HERSHEY_SIMPLEX, 0.5, WHITE, 2)
+                
+                # 원본 영상에 크롭된 부분의 결과 표시
+                orig_xmin, orig_ymin, orig_xmax, orig_ymax = crop_position
+                orig_xmin += xmin
+                orig_ymin += ymin
+                orig_xmax += xmin
+                orig_ymax += ymin
+                cv2.rectangle(self.original_frame, (orig_xmin, orig_ymin), (orig_xmax, orig_ymax), GREEN, 2)
+                cv2.putText(self.original_frame, f'ID: {track.track_id}', (orig_xmin + 5, orig_ymin - 8), cv2.FONT_HERSHEY_SIMPLEX, 0.5, WHITE, 2)
                 
                 for result in results:
                     if np.array_equal(track.to_ltrb(), result[0]):
                         confidence = result[1]
-                        self.csv_writer.writerow([self.frame_cnt, track.track_id, xmin, ymin, xmax, ymax, confidence])
+                        self.csv_writer.writerow([self.frame_cnt, track.track_id, orig_xmin, orig_ymin, orig_xmax, orig_ymax, confidence])
     
     def main(self):
         print('#### [C] main start')
@@ -142,11 +134,14 @@ class ObjectTracker:
             ret, self.original_frame = self.cap.read()
             if not ret:
                 break
+            # 영상에서 좌표를 통해 이미지 잘라서 처리하기 -> 속도 개선을 위함
+            crop_position = self.position_li[-1]
+            frame = img_cropper(self.original_frame, crop_position)
             
-            results = self.human_detection(self.original_frame)
+            results = self.human_detection(frame)
             if results:  # results가 비어 있지 않은지 확인
                 self.position_li.append(results[-1][0])  # 좌표 업데이트
-            self.human_tracker(results, self.original_frame, self.original_frame, self.position_li[-1])
+            self.human_tracker(results, frame, crop_position)
             
             end = datetime.datetime.now()
             fps = self.video_analysis(start, end)
@@ -169,8 +164,8 @@ class ObjectTracker:
 
 if __name__ == '__main__':
     tracker = ObjectTracker(
-        video_path='/home/eiden/eiden/Vvannot/data/demo2-original.mp4',
-        save_path='/home/eiden/eiden/Vvannot/data/demo2-original_res.mp4',
+        video_path='/home/eiden/eiden/Vvannot/data/demo.mp4',
+        save_path='/home/eiden/eiden/Vvannot/data/demo_res.mp4',
         model_path='/home/eiden/eiden/Vvannot/model/weights/yolov8n.pt',
         cocolabel='/home/eiden/eiden/Vvannot/model/weights/coco128.txt',
         confidence_threshold=0.5,
